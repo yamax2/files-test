71680badf48c89d11d1b6c8afbacab0ddba74e28
Maxim Tretyakov <max@tretyakov-ma.ru>
2020-11-20 17:03:12 +0500

    video streaming 200-206

--- lib/app.rb
+++ lib/app.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require 'active_support/core_ext/numeric/bytes'
+require 'active_support/core_ext/enumerable'
+require 'active_support/core_ext/string'
 
 require 'http'
 require 'net/sftp'
@@ -9,20 +11,37 @@ require 'sinatra'
 require 'sinatra/streaming'
 
 class Application < Sinatra::Base
-  helpers Sinatra::Streaming
-
   CHUNK_SIZE = 64.kilobytes
+
+  set :protection, false
 
   get '/' do
     '<h1>Hello!</h1>'
   end
 
   get '/download' do
-    response = HTTP.follow.get('http://tretyakov-ma.ru')
+      proxied_headers = env.
+        select { |key, _| key.starts_with?('HTTP_') && !key.starts_with?('HTTP_X_') }.
+        transform_keys! do |key|
+      ar = key.gsub(/^HTTP_/, '').split('_')
+
+      ar.map! { |x| x.downcase.camelize }.join('-')
+    end
+
+    remote = HTTP.
+      follow.
+      headers(proxied_headers).
+      get('http://192.168.3.4:5002/webdav/travels/2019_05_01%20-%20%D0%9A%D0%B0%D0%B7%D0%B0%D0%BD%D1%8C/10%20-%20%D0%9C%D0%B0%D0%BB%D0%BC%D1%8B%D0%B6-%D0%9A%D0%B0%D0%B7%D0%B0%D0%BD%D1%8C.mp4')
+
+    remote.headers.each { |header, value| response.headers[header] = value }
+
+    status remote.code
 
     stream do |out|
-      response.body.each do |chunk|
-        out.puts chunk
+      remote.body.each do |chunk|
+        break if out.closed?
+
+        out << chunk
       end
     end
   end
--- puma.rb
+++ puma.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-threads_count = ENV.fetch('PUMA_THREADS', 2).to_i
+environment ENV.fetch('RACK_ENV', 'development')
+
+threads_count = ENV.fetch('PUMA_THREADS', 20).to_i
 threads threads_count, threads_count
 
 port ENV.fetch('PORT', 3000).to_i
