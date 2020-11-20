773baa0bc6a8ebc671d530b696d54f45dd0c5a45
Maxim Tretyakov <max@tretyakov-ma.ru>
2020-11-20 18:05:03 +0500

    http proxy fixes

--- lib/app.rb
+++ lib/app.rb
@@ -13,22 +13,21 @@ require 'sinatra/streaming'
 class Application < Sinatra::Base
   CHUNK_SIZE = 64.kilobytes
 
-  set :protection, false
-
   get '/' do
     '<h1>Hello!</h1>'
   end
 
   get '/download' do
-      proxied_headers = env.
-        select { |key, _| key.starts_with?('HTTP_') && !key.starts_with?('HTTP_X_') }.
-        transform_keys! do |key|
+    proxied_headers = env.
+      select { |key, _| key.starts_with?('HTTP_') && !key.starts_with?('HTTP_X_') }.
+      transform_keys! do |key|
       ar = key.gsub(/^HTTP_/, '').split('_')
 
       ar.map! { |x| x.downcase.camelize }.join('-')
     end
 
     remote = HTTP.
+      timeout(3).
       follow.
       headers(proxied_headers).
       get('http://192.168.3.4:5002/webdav/travels/2019_05_01%20-%20%D0%9A%D0%B0%D0%B7%D0%B0%D0%BD%D1%8C/10%20-%20%D0%9C%D0%B0%D0%BB%D0%BC%D1%8B%D0%B6-%D0%9A%D0%B0%D0%B7%D0%B0%D0%BD%D1%8C.mp4')
